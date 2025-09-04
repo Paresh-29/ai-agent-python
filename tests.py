6804c0350cedb56f315feee9c511aa3c07260af5
Paresh-29 <pareshkumarbarick99@gmail.com>
2025-09-04 17:17:11 +0530

    adding a test for writing to a file

--- tests.py
+++ tests.py
@@ -25,6 +25,7 @@ def main():
     # tests for writing in file
     print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
     print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file(working_dir, "pkg2/morelorem.txt", "lorem ipsum dolor sit amet"))
     print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
 
 
