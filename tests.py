98a14b08fa8638ee5713dc3f61bd90f2f539af95
Paresh-29 <pareshkumarbarick99@gmail.com>
2025-09-05 16:52:58 +0530

    Run python file

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 def main():
@@ -23,10 +24,23 @@ def main():
     # print(get_file_content(working_dir, "/bin/cat"))
 
     # tests for writing in file
-    print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
-    print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-    print(write_file(working_dir, "pkg2/morelorem.txt", "lorem ipsum dolor sit amet"))
-    print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
+    # print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
+    # print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    # print(write_file(working_dir, "pkg2/morelorem.txt", "lorem ipsum dolor sit amet"))
+    # print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
+
+    #  (should print the calculator's usage instructions)
+    print(run_python_file(working_dir, "main.py"))
+    # (should run the calculator... which gives a kinda nasty rendered result)
+    print(run_python_file(working_dir, "main.py", ["3 + 5"]))
+
+    print(run_python_file(working_dir, "tests.py"))
+
+    #  (this should return an error)
+    print(run_python_file(working_dir, "../main.py"))
+
+    # (this should return an error)
+    print(run_python_file(working_dir, "nonexistent.py"))
 
 
 main()
