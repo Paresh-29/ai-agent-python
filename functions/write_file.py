e667a73734b77bb38bb04f32cf20b73ba6cdd566
Paresh-29 <pareshkumarbarick99@gmail.com>
2025-09-06 15:48:29 +0530

    adding more functions

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 
 def write_file(working_directory, file_path, content):
@@ -26,3 +27,22 @@ def write_file(working_directory, file_path, content):
         )
     except Exception as e:
         return f"Failed to write to file:{file_path},{e}"
+
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Overwrites an existing file or writes to a new file if it doesn't exist (and create required parent directories safely), constrained to the working directory",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write",
+            ),
+            "contents": types.Schema(
+                type=types.Type.STRING,
+                description="The contents to write to the file as a string",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from google import genai
 from google.genai import types
 from functions.get_files_info import get_files_info
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
 
 
 def main():
@@ -20,6 +23,9 @@ def main():
     When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
     - List files and directories
+    - Read the contents of a file
+    - write to a file (create or update)
+    - Run a python file with optional arguments
 
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
@@ -38,7 +44,14 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
-    available_functions = types.Tool(function_declarations=[schema_get_files_info])
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file,
+        ]
+    )
 
     config = types.GenerateContentConfig(
         tools=[available_functions], system_instruction=system_prompt
